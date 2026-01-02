42c7f64ec011d85e82a4051889a1c6c9408f4222
Eyasu Kassa <eyasubiyaylgn@gmail.com>
2026-01-02 20:02:47 +0200

    modified protocol.txt and .gitignore and added a new .py file containing constant variables (constants.py)

---
+++ constants.py
@@ -0,0 +1,15 @@
+#this file contains the declaration contant variables used by the client and the server.
+
+#commands:
+SUCESS, R_DOWNLOAD, R_LIST, ERROR= "success", "r_download", "r_list", "error"
+UPLOAD, DOWNLOAD, LIST, DELETE = "upload", "download", "list", "delete"
+CLIENT_COMMANDS = (UPLOAD, DOWNLOAD, LIST, DELETE)
+RESPONSES = (SUCESS, R_DOWNLOAD, R_LIST, ERROR)
+
+#error codes:
+INVALID_COMMAND, FILE_EXISTS, FILE_NOT_FOUND = 0, 1, 2
+
+
+DELIMITER = ':'
+FILE_ATTRIBUTE_DELIMITER = ';'
+DEFAULT_BUFFER_SIZE = 1024
