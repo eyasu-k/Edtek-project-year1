7ebb8b70f0ace9f074a7526123838978371ce852
Eyasu Kassa <eyasubiyaylgn@gmail.com>
2026-01-02 21:51:01 +0200

    modified constants.py and implemented all file modification functions to server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import constants as const
+import file_manager as explorer
+from constants import SERVER_FILES_FOLDER_NAME, SUCESS, FILES_DELIMITER
 
 PORT = 12345
 DEBUG_PRINTS = False
@@ -8,17 +10,33 @@ def debug(*msg)-> None:
     if DEBUG_PRINTS:
         print("Debug print: "*msg)
 
+def receive_file(client: socket.socket, file_name: str, file_size: str, *_)-> None:
+    file_path = SERVER_FILES_FOLDER_NAME+'/'+file_name
+    explorer.create_new_folder(const.SERVER_FILES_FOLDER_NAME)
+    file_content = client.recv(int(file_size))
+    explorer.create_file(file_path, file_content)
+    client.sendall(SUCESS)
 
-def foo(*_)-> any:#placeholder
-    pass
+def send_file(client: socket.socket, file_name: str, *_)-> None:
+    file_path = SERVER_FILES_FOLDER_NAME+'/'+file_name
+    file_contents = explorer.get_file(file_path)
+    client.sendall(file_contents)
+
+def send_file_list(client: socket.socket, *_)-> None:
+    files = explorer.get_files_list(SERVER_FILES_FOLDER_NAME)
+    files = FILES_DELIMITER.join(files)
+    client.sendall(files.encode())
+
+def delete_file(client: socket.socket, file_name: str,*_)-> None:
+    file_path = SERVER_FILES_FOLDER_NAME+'/'+file_name
+    explorer.delete_file(file_path)
 
 def serve_client(client: socket.socket)-> bool:#TODO: add exceptions
-    actions = {const.UPLOAD: foo, const.DOWNLOAD: foo, const.LIST: foo, const.DELETE: foo}
+    actions = {const.UPLOAD: receive_file, const.DOWNLOAD: send_file, const.LIST: send_file_list, const.DELETE: delete_file}
     try:
         new_request = client.recv(const.DEFAULT_BUFFER_SIZE)
         command, *parameters = new_request.split(const.DELIMITER)
-        response = actions[command](parameters)
-        client.sendall(response.encode())
+        actions[command](client, parameters)
         return True
     except:
         return False #the client disconnected or there is an error in the server
