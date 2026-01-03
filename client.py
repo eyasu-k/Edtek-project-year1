b4c74bc93d3592dc744fcac0072df4979179970e
Eyasu Kassa <eyasubiyaylgn@gmail.com>
2026-01-03 13:37:36 +0200

    finished working on the skeleton of client.py and implemented a function to upload a file.

---
+++ client.py
@@ -0,0 +1,39 @@
+import socket
+import constants as const
+import file_manager as explorer
+
+DEBUG_PRINTS = True
+IP = "127.0.0.1"
+PORT = 12345
+
+DEFAULT_DOWNLOAD_DST = explorer.get_downloads_path()
+
+
+def debug(*msg)-> None:
+    if DEBUG_PRINTS:
+        print("Debug print: ", *msg)
+
+def upload_file(server: socket.socket, file_path: str)-> None:
+    file = explorer.get_file(file_path)
+    file_size = len(file)
+    file_name = explorer.get_file_name(file_path)
+    request = const.DELIMITER.join([const.UPLOAD, file_name, file_size])
+    server.sendall(request)
+    server.sendall(file)
+
+def connect_to_server(ip: str, port: int)-> socket.socket:
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((ip, port))
+        return client_socket
+    except Exception as e:
+        print("Error connecting to the server. more details:\n", e)
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
