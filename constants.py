e8e981ae56d7a520712f5732cf25be44ff4643db
Eyasu Kassa <eyasubiyaylgn@gmail.com>
2026-01-03 12:42:19 +0200

    implemented exception printing for in server.py and fixed a spelling mistake inside constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 #this file contains the declaration contant variables used by the client and the server.
 
 #commands:
-SUCESS, R_DOWNLOAD, R_LIST, ERROR= "success", "r_download", "r_list", "error"
+SUCCESS, R_DOWNLOAD, R_LIST, ERROR= "success", "r_download", "r_list", "error"
 UPLOAD, DOWNLOAD, LIST, DELETE = "upload", "download", "list", "delete"
 CLIENT_COMMANDS = (UPLOAD, DOWNLOAD, LIST, DELETE)
-RESPONSES = (SUCESS, R_DOWNLOAD, R_LIST, ERROR)
+RESPONSES = (SUCCESS, R_DOWNLOAD, R_LIST, ERROR)
 
 #error codes:
 INVALID_COMMAND, FILE_EXISTS, FILE_NOT_FOUND = 0, 1, 2
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import constants as const
 import file_manager as explorer
-from constants import SERVER_FILES_FOLDER_NAME, SUCESS, FILES_DELIMITER
 
 PORT = 12345
 DEBUG_PRINTS = False
@@ -11,24 +10,24 @@ def debug(*msg)-> None:
         print("Debug print: ", *msg)
 
 def receive_file(client: socket.socket, file_name: str, file_size: str, *_)-> None:
-    file_path = SERVER_FILES_FOLDER_NAME+'/'+file_name
+    file_path = const.SERVER_FILES_FOLDER_NAME+'/'+file_name
     explorer.create_new_folder(const.SERVER_FILES_FOLDER_NAME)
     file_content = client.recv(int(file_size))
     explorer.create_file(file_path, file_content)
-    client.sendall(SUCESS)
+    client.sendall(const.SUCCESS)
 
 def send_file(client: socket.socket, file_name: str, *_)-> None:
-    file_path = SERVER_FILES_FOLDER_NAME+'/'+file_name
+    file_path = const.SERVER_FILES_FOLDER_NAME+'/'+file_name
     file_contents = explorer.get_file(file_path)
     client.sendall(file_contents)
 
 def send_file_list(client: socket.socket, *_)-> None:
-    files = explorer.get_files_list(SERVER_FILES_FOLDER_NAME)
-    files = FILES_DELIMITER.join(files)
+    files = explorer.get_files_list(const.SERVER_FILES_FOLDER_NAME)
+    files = const.FILES_DELIMITER.join(files)
     client.sendall(files.encode())
 
 def delete_file(client: socket.socket, file_name: str,*_)-> None:
-    file_path = SERVER_FILES_FOLDER_NAME+'/'+file_name
+    file_path = const.SERVER_FILES_FOLDER_NAME+'/'+file_name
     explorer.delete_file(file_path)
 
 def serve_client(client: socket.socket)-> bool:#TODO: add exceptions
@@ -38,7 +37,8 @@ def serve_client(client: socket.socket)-> bool:#TODO: add exceptions
         command, *parameters = new_request.split(const.DELIMITER)
         actions[command](client, parameters)
         return True
-    except:
+    except Exception as e:
+        print("Internal server error:\n", e)
         return False #the client disconnected or there is an error in the server
     
     
@@ -52,7 +52,8 @@ def start_server()-> bool:#TODO: add exceptions if necessary...
             while serve_client(client):
                 continue
         return True
-    except:
+    except Exception as e:
+        print("Error starting the server. more details:\n", e)
         return False
 
 def main():
