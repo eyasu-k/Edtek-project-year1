ef7a3b09c07fc51dcada2ade503acc6b3ffa18b7
Eyasu Kassa <eyasubiyaylgn@gmail.com>
2026-01-06 14:36:30 +0200

    returned back the exception checking to the server

--- server.py
+++ server.py
@@ -41,31 +41,31 @@ def delete_file(_client: socket.socket, file_name: str,*_)-> None:
 
 def serve_client(client: socket.socket)-> bool:
     actions = {const.UPLOAD: receive_file, const.DOWNLOAD: send_file, const.LIST: send_file_list, const.DELETE: delete_file}
-# try:
-    new_request = client.recv(const.DEFAULT_BUFFER_SIZE).decode()
-    debug("New client request:", new_request)
-    command, *parameters = new_request.split(const.DELIMITER)
-    actions[command](client, *parameters)
-    return True
-# except Exception as e:
-#     print("Internal server error:\n", e)
-#     return False #the client disconnected or there is an error in the server
+    try:
+        new_request = client.recv(const.DEFAULT_BUFFER_SIZE).decode()
+        debug("New client request:", new_request)
+        command, *parameters = new_request.split(const.DELIMITER)
+        actions[command](client, *parameters)
+        return True
+    except Exception as e:
+        print("Internal server error:\n", e)
+        return False #the client disconnected or there is an error in the server
     
     
     
 def start_server()-> bool:
-# try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as new_socket:
-        new_socket.bind(("", PORT))
-        new_socket.listen(1)
-        client, _ = new_socket.accept()
-        debug("Connected to the client.")
-        while serve_client(client):
-            continue
-    return True
-# except Exception as e:
-#     print("Error starting the server. more details:\n", e)
-#     return False
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as new_socket:
+            new_socket.bind(("", PORT))
+            new_socket.listen(1)
+            client, _ = new_socket.accept()
+            debug("Connected to the client.")
+            while serve_client(client):
+                continue
+        return True
+    except Exception as e:
+        print("Error starting the server. more details:\n", e)
+        return False
 
 def main():
     while start_server():#nice
